7e1f9c5e02293c5577f0e72a6c197ecb81314b39
Adar Azoulay <12azoulay12@gmail.com>
2021-11-21 17:34:37 +0200

    TEST CHECK

--- tictactoe.js
+++ tictactoe.js
@@ -14,13 +14,117 @@
 //recieve a string array parameter and return true
 //if the game is a draw or false if not.
 function hasDraw(selectionArray) {
-   return false;
+if (hasWon(selectionArray)==true)
+return false;
+
+   return true;
 }
 //recieve a string array parameter and return true 
 //if someone won the game or false if not.
 function hasWon(selectionArray) {
+    if(
+        xHorizontallyWon(selectionArray) ||
+        xVerticallyWon(selectionArray) ||
+        xDiagonallyWon(selectionArray) ||
+        oHorizontallyWon(selectionArray) ||
+        oVerticallyWon(selectionArray) ||
+        oDiagonallyWon(selectionArray) 
+        )
+        return true;
+        
+        
     return false;
 }
+
+function xHorizontallyWon(selectionArray){
+if(
+    (selectionArray[0]=='x' && selectionArray[1]=='x' && selectionArray[2]=='x'))
+    return true;
+
+if(
+    (selectionArray[3]=='x' && selectionArray[4]=='x' && selectionArray[5]=='x') )
+    return true;
+if(
+    (selectionArray[6]=='x' && selectionArray[7]=='x' && selectionArray[8]=='x') )
+    return true;
+
+    return false;
+}
+function oHorizontallyWon(selectionArray){
+    if(
+        (selectionArray[0]=='o' && selectionArray[1]=='o' && selectionArray[2]=='o'))
+        return true;
+    
+    if(
+        (selectionArray[3]=='o' && selectionArray[4]=='o' && selectionArray[5]=='o'))
+        return true;
+    if(
+        (selectionArray[6]=='o' && selectionArray[7]=='o' && selectionArray[8]=='o'))
+        return true;
+    
+        return false;
+    }
+
+
+
+
+function xVerticallyWon(selectionArray){
+    if(
+        (selectionArray[0]=='x' && selectionArray[3]=='x' && selectionArray[6]=='x') )
+        return true;
+    
+    if(
+        (selectionArray[1]=='x' && selectionArray[4]=='x' && selectionArray[7]=='x') )
+        return true;
+    if(
+        (selectionArray[2]=='x' && selectionArray[5]=='x' && selectionArray[8]=='x') )
+        return true;
+    
+        return false;
+    }
+
+    function oVerticallyWon(selectionArray){
+        if(
+            (selectionArray[0]=='o' && selectionArray[3]=='o' && selectionArray[6]=='o') )
+            return true;
+        
+        if(
+            (selectionArray[1]=='o' && selectionArray[4]=='o' && selectionArray[7]=='o') )
+            return true;
+        if(
+            (selectionArray[2]=='o' && selectionArray[5]=='o' && selectionArray[8]=='o') )
+            return true;
+        
+            return false;
+        }
+
+    function xDiagonallyWon(selectionArray){
+        if(
+            (selectionArray[0]=='x' && selectionArray[4]=='x' && selectionArray[8]=='x') )
+            return true;
+        
+        if(
+            (selectionArray[2]=='x' && selectionArray[4]=='x' && selectionArray[6]=='x') )
+            return true;
+
+        
+            return false;
+        }
+        function oDiagonallyWon(selectionArray){
+            if(
+                (selectionArray[0]=='o' && selectionArray[4]=='o' && selectionArray[8]=='o'))
+                return true;
+            
+            if(
+                (selectionArray[2]=='o' && selectionArray[4]=='o' && selectionArray[6]=='o'))
+                return true;
+    
+            
+                return false;
+            }
+    
+
+
 //Please , don't modify the following code
 if (typeof module !== "undefined") {
     module.exports = {
