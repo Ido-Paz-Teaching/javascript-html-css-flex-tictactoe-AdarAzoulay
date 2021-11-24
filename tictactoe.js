f0c8b1f263e09c1473a06cfe72f981cb1959ad4d
Adar Azoulay <12azoulay12@gmail.com>
2021-11-24 11:38:24 +0200

    Work Done

--- tictactoe.js
+++ tictactoe.js
@@ -14,117 +14,70 @@
 //recieve a string array parameter and return true
 //if the game is a draw or false if not.
 function hasDraw(selectionArray) {
-if (hasWon(selectionArray)==true)
-return false;
 
-   return true;
+    if ( AllcellsHaveValue(selectionArray)==true && !hasWon(selectionArray))
+    return true;
+    return false}
+
+function AllcellsHaveValue(selectionArray){
+    if (selectionArray[0] !== " "  &&
+    selectionArray[1] !== " " && 
+    selectionArray[2] !== " " && 
+    selectionArray[3] !== " " && 
+    selectionArray[4] !== " " && 
+    selectionArray[5] !== " " && 
+    selectionArray[6] !== " " && 
+    selectionArray[7] !== " " && 
+    selectionArray[8] !== " " )
+    return true;
+
+
 }
+
 //recieve a string array parameter and return true 
 //if someone won the game or false if not.
 function hasWon(selectionArray) {
-    if(
-        xHorizontallyWon(selectionArray) ||
-        xVerticallyWon(selectionArray) ||
-        xDiagonallyWon(selectionArray) ||
-        oHorizontallyWon(selectionArray) ||
-        oVerticallyWon(selectionArray) ||
-        oDiagonallyWon(selectionArray) 
-        )
-        return true;
-        
-        
+    
+    if(Somewin(selectionArray))
+    return true;
     return false;
 }
 
-function xHorizontallyWon(selectionArray){
-if(
-    (selectionArray[0]=='x' && selectionArray[1]=='x' && selectionArray[2]=='x'))
-    return true;
-
-if(
-    (selectionArray[3]=='x' && selectionArray[4]=='x' && selectionArray[5]=='x') )
-    return true;
-if(
-    (selectionArray[6]=='x' && selectionArray[7]=='x' && selectionArray[8]=='x') )
-    return true;
-
-    return false;
+function Vertical(selectionArray){
+    for(let i=0; i<3; i++){ 
+        if( selectionArray[i] === selectionArray[i+3] && selectionArray[i] === selectionArray[i+6] && selectionArray[i] !== undefined && selectionArray[i] !== " "){
+            return true;
+        }
 }
-function oHorizontallyWon(selectionArray){
-    if(
-        (selectionArray[0]=='o' && selectionArray[1]=='o' && selectionArray[2]=='o'))
+}
+ 
+function Horizontal(selectionArray){
+    for(let i=0; i<7; i+=3){ 
+        if(selectionArray[i] !== undefined && selectionArray[i] === selectionArray[i+1] && selectionArray[i] === selectionArray[i+2] && selectionArray[i] !== " "){
+            return true;
+        }
+    }
+}
+
+function Otherways(selectionArray){
+    if(selectionArray[0] !== undefined && selectionArray[0] === selectionArray[4] && selectionArray[0] === selectionArray[8] && selectionArray[0] !== " "){
         return true;
-    
-    if(
-        (selectionArray[3]=='o' && selectionArray[4]=='o' && selectionArray[5]=='o'))
+    }
+    else if(selectionArray[2] !== undefined && selectionArray[2] === selectionArray[4] && selectionArray[2] === selectionArray[6] && selectionArray[2] !== " "){
         return true;
-    if(
-        (selectionArray[6]=='o' && selectionArray[7]=='o' && selectionArray[8]=='o'))
-        return true;
-    
-        return false;
     }
 
+}
+
+function Somewin(selectionArray){
+    if (Vertical(selectionArray)|| Horizontal(selectionArray) || Otherways(selectionArray) )
+    return true;
+    else return false;
+}
 
 
-
-function xVerticallyWon(selectionArray){
-    if(
-        (selectionArray[0]=='x' && selectionArray[3]=='x' && selectionArray[6]=='x') )
-        return true;
-    
-    if(
-        (selectionArray[1]=='x' && selectionArray[4]=='x' && selectionArray[7]=='x') )
-        return true;
-    if(
-        (selectionArray[2]=='x' && selectionArray[5]=='x' && selectionArray[8]=='x') )
-        return true;
-    
-        return false;
-    }
-
-    function oVerticallyWon(selectionArray){
-        if(
-            (selectionArray[0]=='o' && selectionArray[3]=='o' && selectionArray[6]=='o') )
-            return true;
-        
-        if(
-            (selectionArray[1]=='o' && selectionArray[4]=='o' && selectionArray[7]=='o') )
-            return true;
-        if(
-            (selectionArray[2]=='o' && selectionArray[5]=='o' && selectionArray[8]=='o') )
-            return true;
-        
-            return false;
-        }
-
-    function xDiagonallyWon(selectionArray){
-        if(
-            (selectionArray[0]=='x' && selectionArray[4]=='x' && selectionArray[8]=='x') )
-            return true;
-        
-        if(
-            (selectionArray[2]=='x' && selectionArray[4]=='x' && selectionArray[6]=='x') )
-            return true;
 
         
-            return false;
-        }
-        function oDiagonallyWon(selectionArray){
-            if(
-                (selectionArray[0]=='o' && selectionArray[4]=='o' && selectionArray[8]=='o'))
-                return true;
-            
-            if(
-                (selectionArray[2]=='o' && selectionArray[4]=='o' && selectionArray[6]=='o'))
-                return true;
-    
-            
-                return false;
-            }
-    
-
-
 //Please , don't modify the following code
 if (typeof module !== "undefined") {
     module.exports = {
--- tictactoeUI.js
+++ tictactoeUI.js
@@ -1,6 +1,106 @@
-/*
-enter code to handle:
-1. User cell selection click
-2. game status update messages
-3. Start button click
-*/
+
+window.onload = function(){
+    // Declare variables
+        const boardArray = [" "," "," "," "," "," "," "," "," ",]
+        const b0 = document.getElementById("b0");
+        const b1 = document.getElementById("b1");
+        const b2 = document.getElementById("b2");
+        const b3 = document.getElementById("b3");
+        const b4 = document.getElementById("b4");
+        const b5 = document.getElementById("b5");
+        const b6 = document.getElementById("b6");
+        const b7 = document.getElementById("b7");
+        const b8 = document.getElementById("b8");
+        const message = document.getElementById("gameStatusTag");
+        let flag = 'x';
+        let turnnumbers = 0;
+        let Gameover=false;
+    //Declare functions
+
+        function startgame(){
+        document.querySelector('header button').innerHTML = "Reset"
+        document.querySelector('header button').addEventListener('click', restartgame);
+        message.innerHTML = 'waiting for X first move';
+        b0.addEventListener('click', selectcell);
+        b1.addEventListener('click', selectcell);
+        b2.addEventListener('click', selectcell);
+        b3.addEventListener('click', selectcell);
+        b4.addEventListener('click', selectcell);
+        b5.addEventListener('click', selectcell);
+        b6.addEventListener('click', selectcell);
+        b7.addEventListener('click', selectcell);
+        b8.addEventListener('click', selectcell);
+        }
+
+        function selectcell(event){
+        let selectedcell = event.currentTarget; //#button b0
+        let selectedcellID = event.target.id; //"b0"
+        if( !Gameover )
+        {
+            if(selectedcell.innerHTML == ""){
+            switch(flag){
+            case 'x':
+                selectedcell.innerHTML = 'x';
+                boardArray[selectedcellID[1]] = 'x';
+                //turnnumbers++;
+                if(hasWon(boardArray))
+            {
+                Gameover = true;
+                statusgame();
+                break;
+            }
+                flag = 'o';
+                statusgame();
+                break;
+
+            case 'o':
+                selectedcell.innerHTML = 'o';
+                boardArray[selectedcellID[1]] = 'o';
+               // turnnumbers++;
+                if(hasWon(boardArray))
+            {
+                Gameover = true;
+                statusgame();
+                break;
+            }
+                flag = 'x';
+                statusgame();
+                break;
+            }
+            }
+            }
+        }
+        function restartgame(){
+            location.reload();
+        }
+    
+        function statusgame(){
+            if (Gameover){
+            message.innerHTML = `${flag} won the game`;
+            disabled()}
+            else if (hasDraw(boardArray)){         //else if turnnumbers==9 && !hasWon(boardArray){           
+            message.innerHTML = "IT'S A DRAW!";{
+            Gameover = true;
+            disabled()}
+        }
+            else{
+                message.innerHTML = `it's ${flag} turn`
+            }
+        }
+        
+        function disabled(){
+            b0.disabled = true;
+            b1.disabled = true;
+            b2.disabled = true;
+            b3.disabled = true;
+            b4.disabled = true;
+            b5.disabled = true;
+            b6.disabled = true;
+            b7.disabled = true;
+            b8.disabled = true;
+        }
+
+        
+    // Start the game
+        startgame();
+}
